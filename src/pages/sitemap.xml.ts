f96435dae79c40936f22e55d44f8c1b3529f17a3
wilsonlvin007 <wilsonlvin7@gmail.com>
2026-06-17 08:21:23 +0800

    fix: avoid template literals with XML in .ts file

--- src/pages/sitemap.xml.ts
+++ src/pages/sitemap.xml.ts
@@ -4,37 +4,40 @@ export async function GET() {
   const dates = getAllDates();
   const baseUrl = 'https://ailens.news';
 
-  const urls = [
-    `  <url>
-    <loc>${baseUrl}/</loc>
-    <changefreq>daily</changefreq>
-    <priority>1.0</priority>
-  </url>`,
-    `  <url>
-    <loc>${baseUrl}/archive</loc>
-    <changefreq>daily</changefreq>
-    <priority>0.8</priority>
-  </url>`,
-    `  <url>
-    <loc>${baseUrl}/subscribe</loc>
-    <changefreq>monthly</changefreq>
-    <priority>0.4</priority>
-  </url>`,
-    ...dates.map(date => `  <url>
-    <loc>${baseUrl}/${date}</loc>
-    <lastmod>${date}</lastmod>
-    <changefreq>weekly</changefreq>
-    <priority>0.6</priority>
-  </url>`),
+  const lines = [
+    '<?xml version="1.0" encoding="UTF-8"?>',
+    '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">',
+    '  <url>',
+    '    <loc>' + baseUrl + '/</loc>',
+    '    <changefreq>daily</changefreq>',
+    '    <priority>1.0</priority>',
+    '  </url>',
+    '  <url>',
+    '    <loc>' + baseUrl + '/archive</loc>',
+    '    <changefreq>daily</changefreq>',
+    '    <priority>0.8</priority>',
+    '  </url>',
+    '  <url>',
+    '    <loc>' + baseUrl + '/subscribe</loc>',
+    '    <changefreq>monthly</changefreq>',
+    '    <priority>0.4</priority>',
+    '  </url>',
   ];
 
-  const xml = `<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-${urls.join('
-')}
-</urlset>`;
+  for (const date of dates) {
+    lines.push(
+      '  <url>',
+      '    <loc>' + baseUrl + '/' + date + '</loc>',
+      '    <lastmod>' + date + '</lastmod>',
+      '    <changefreq>weekly</changefreq>',
+      '    <priority>0.6</priority>',
+      '  </url>'
+    );
+  }
 
-  return new Response(xml, {
+  lines.push('</urlset>');
+
+  return new Response(lines.join('\n'), {
     headers: { 'Content-Type': 'application/xml; charset=utf-8' },
   });
 }
